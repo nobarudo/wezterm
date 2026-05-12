fa428efc58bdc4acfe10e5b30e0ccc05e81445b5
nobarudo <esukab@gmail.com>
2026-05-13 01:01:47 +0900

    feat:ペイン削除のキーバインドの追加

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,5 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-local act = wezterm.action
-local mux = wezterm.mux
 
 -- 基本設定
 config.automatically_reload_config = true
@@ -12,10 +10,22 @@ config.color_scheme = "Tokyo Night"
 config.font_size = 10
 config.use_ime = true
 
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
+
 config.colors = {
 	tab_bar = {
 		inactive_tab_edge = "none",
 	},
 }
+
+local keys = {}
+
+table.insert(keys, {
+	key = "d",
+	mods = "CTRL|SHIFT",
+	action = wezterm.action.CloseCurrentPane({ confirm = false }),
+})
 
 return config
