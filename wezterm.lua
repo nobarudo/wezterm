cdd2fd2f1583efcbdd97220cb786fad403b861e1
nobarudo <esukab@gmail.com>
2026-05-16 00:32:28 +0900

    feat:カラースキームの変更とタブの見た目の変更

--- wezterm.lua
+++ wezterm.lua
@@ -5,15 +5,16 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = true
 config.window_close_confirmation = "NeverPrompt"
 config.default_cursor_style = "BlinkingBar"
-config.color_scheme = "Tokyo Night Moon"
+config.color_scheme = "nord"
 
 config.font_size = 10
 config.use_ime = true
 
 config.use_fancy_tab_bar = false
-config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
 config.show_new_tab_button_in_tab_bar = false
 config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.95
 
 config.colors = {
 	tab_bar = {
@@ -28,5 +29,11 @@ table.insert(keys, {
 	mods = "CTRL|SHIFT",
 	action = wezterm.action.CloseCurrentPane({ confirm = false }),
 })
+
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+tabline.setup({
+	options = { theme = "nord" },
+})
+tabline.setup()
 
 return config
