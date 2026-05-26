ec4519a0ee06e40f7ec330028a1d22962a636fa8
nobarudo <esukab@gmail.com>
2026-05-26 19:52:55 +0900

    feat:タブ関連のキーマップの追加

--- wezterm.lua
+++ wezterm.lua
@@ -29,6 +29,20 @@ config.colors = {
 
 config.keys = {
 	{
+		key = "%",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "#",
+		mods = "CTRL|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{ key = "h", mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "CTRL|SHIFT", action = wezterm.action.ActivatePaneDirection("Right") },
+	{
 		mods = "CTRL|SHIFT",
 		key = "s",
 		action = act.ShowLauncherArgs({ flags = "WORKSPACES", title = "Select workspace" }),
